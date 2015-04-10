08470a30c223800f8f3ccf9c1d0ce5fc1e61cbbe
Alberto Gragera <alberto@gokarumi.com>
2015-04-10 22:50:44 +0200

    Initial Commit

---
+++ FoursquareTop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FoursquareTop
+//
+//  Created by Alberto Gragera Cerrajero on 10/04/15.
+//  Copyright (c) 2015 Karumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FoursquareTop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FoursquareTop
+//
+//  Created by Alberto Gragera Cerrajero on 10/04/15.
+//  Copyright (c) 2015 Karumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
