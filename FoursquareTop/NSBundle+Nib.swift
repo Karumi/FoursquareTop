c065ac34980253ee28fdf8ee65a48286f25c85fd
Fran Fernandez <fran@gokarumi.com>
2016-09-20 17:00:44 +0200

    Add support to Swift 2.3
    Updated Podfile to use the right dependencies.
    Converted project using Xcode conversion support.

--- FoursquareTop/NSBundle+Nib.swift
+++ FoursquareTop/NSBundle+Nib.swift
@@ -3,10 +3,10 @@ import UIKit
 
 extension NSBundle {
     public func loadNib<T: UIView>(name name: String, owner: AnyObject?) -> T {
-        return self.loadNibNamed(name, owner: owner, options: nil)[0] as! T
+        return self.loadNibNamed(name, owner: owner, options: nil)![0] as! T
     }
     
     public func loadNib<T: UIView>(name name: String) -> T {
-        return self.loadNibNamed(name, owner: nil, options: nil)[0] as! T
+        return self.loadNibNamed(name, owner: nil, options: nil)![0] as! T
     }
 }
